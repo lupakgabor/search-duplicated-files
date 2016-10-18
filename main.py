e46b87de9667933bf28fa93f50c62ab76cf5c5b7
Lupák Gábor <lupakgabor@Lupak-MacBook-Air.local>
2016-10-18 11:22:06 +0200

    delete duplicated file

--- main.py
+++ main.py
@@ -5,30 +5,31 @@ from time import sleep
 
 if __name__ == '__main__':
     start = timeit.default_timer()
-    walk_dir = '/Volumes/My Passport/BackUP/Google Drive/HDD Backup/' 
+    walk_dir = '/Users/lupakgabor/Documents/duplicate_test/' 
     all_files = {}
+    duplicated_found = False;
     for root, subdirs, files in os.walk(walk_dir):
-        #print(root)
-        #print('--\nroot = ' + str(subdirs))
         for file in files:
             if file not in all_files:
                 all_files[file] = []
                 all_files[file].append(root + '/' + str(file))
             else:
-                # todo ugyan az a file ?
                 new_files = []
                 for duplicated_file in all_files[file]:
                     temp_path = root + '/' + str(file)
                     os.stat(duplicated_file)
                     os.stat(temp_path)
                     if filecmp.cmp(duplicated_file, temp_path):
+                        duplicated_found = True;
                         print('Duplicated file found:')
                         print(duplicated_file)
-                        print(temp_path)
+                        print(temp_path+' DELETED')
+                        os.remove(temp_path)
                     else:
                         new_files.append(temp_path)
                 all_files[file] = all_files[file] + new_files
-
+    if not duplicated_found:
+        print('Duplicated files not found')
     print("END")
     stop = timeit.default_timer()
     m, s = divmod(stop - start, 60)
